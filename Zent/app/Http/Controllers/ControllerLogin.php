79f1d77f2f8590a8db10cac9ae3d86661708f71c
Adrian Angulo <anguloadrian2003@gmail.com>
2026-01-26 16:37:39 +0100

    prueba_login_controlador
    Ahora /login esta en un controlador

--- Zent/app/Http/Controllers/ControllerLogin.php
+++ Zent/app/Http/Controllers/ControllerLogin.php
@@ -4,10 +4,27 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class ControllerLogin extends Controller
 {
-    public function login(){
-        return view('login');
+    function login(Request $request){
+
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            return response()->json(['error' => 'Credenciales incorrectas'], 401);
+        }
+
+        $token = $user->createToken('token_prueba')->plainTextToken;
+
+        return response()->json([
+            'token' => $token
+        ]);
     }
 }
--- Zent/routes/api.php
+++ Zent/routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Hash;
-use App\Models\User;
+
 
 
 /*
@@ -17,30 +17,8 @@ use App\Models\User;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::middleware('auth:sanctum')->get('/perfil', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/login', function(Request $request){
-
-    $request->validate([
-        'email' => 'required|email',
-        'password' => 'required'
-    ]);
-
-    $user = User::where('email', $request->email)->first();
-
-    if (! $user || ! Hash::check($request->password, $user->password)) {
-        return response()->json(['error' => 'Credenciales incorrectas'], 401);
-    }
-
-    $token = $user->createToken('token_prueba')->plainTextToken;
-
-    return response()->json([
-        'token' => $token
-    ]);
-});
+Route::post('/login', [ControllerLogin::class,'login'])->name('login');
