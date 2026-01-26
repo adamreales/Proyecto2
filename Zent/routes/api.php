612cbe6ec0d078c718b0d42e65291699aa2ddbf5
Adrian Angulo <anguloadrian2003@gmail.com>
2026-01-26 16:10:59 +0100

    primera_prueba_postman

--- Zent/routes/api.php
+++ Zent/routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +19,28 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware('auth:sanctum')->get('/perfil', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('/login', function(Request $request){
+
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required'
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        return response()->json(['error' => 'Credenciales incorrectas'], 401);
+    }
+
+    $token = $user->createToken('token_prueba')->plainTextToken;
+
+    return response()->json([
+        'token' => $token
+    ]);
 });
