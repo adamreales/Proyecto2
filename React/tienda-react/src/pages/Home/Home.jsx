d8d631231bb44e1ee017c165cd3dd025549d041f
adamreales <adam.br2003@gmail.com>
2026-02-05 17:36:36 +0100

    Mejora Home
    Mostramos productos y bien diseñados

--- React/tienda-react/src/pages/Home/Home.jsx
+++ React/tienda-react/src/pages/Home/Home.jsx
@@ -5,18 +5,33 @@ import {Link} from "react-router-dom";
 function Home(){
 	const [masVendidos,setMasvendidos] = useState([]);
 	const [masValorados,setMasvalorados] = useState([]);
-
+	const [productos,setProductos] = useState([]);
 	useEffect(()=>
 	{
 		fetch("http://127.0.0.1:8000/api/productos_mas_vendidos")
 		.then(res=>res.json())
 		.then(data => {
-			console.log(data);
-        setMasvendidos(data.masVendidos);
-		setMasvalorados(data.masValorados);
+       		setMasvendidos(data.masVendidos);
 		})
 		.catch(err => console.error(err));
 	}, []);
+
+	useEffect(() => 
+	{
+		fetch("http://127.0.0.1:8000/api/productos_mas_populares")
+		.then(res => res.json())
+		.then(data => {
+			setMasvalorados(data.masPopulares);
+		})
+	},[]);
+	useEffect(() => 
+	{
+		fetch("http://127.0.0.1:8000/api/productos")
+		.then(res => res.json())
+		.then(data => {
+			setProductos(data.productos);
+		})
+	},[]);
     return (
         <> 
         <article className="portada">
@@ -33,35 +48,58 @@ function Home(){
 		<main>
 			<h2 className="subtitulo">Los Mas Vendidos</h2>
 			<section className="MasVendidos">
-				{masVendidos.slice(0, 5).map(p => (
+				{masVendidos.slice(0, 3).map(p => (
 				<div className="producto-catalogo" key={p.id}>
 					<div className="imagen-producto">
-						<img src={`http://127.0.0.1:8000/storage/${p.do_imagenes.url}`}/>
+						<img
+							src={`http://zent.es/${p.do_imagenes[0].url}`}
+							alt={p.titulo}
+						/>
 					</div>
 					<div className="info-producto">
 						<h3>{p.titulo}</h3>
-						<p>${p.precio}</p>
+						<p>{p.precio} €</p>
 					</div>
 				</div>
 				))}
 			</section>
-			{/* <h2 className="subtitulo">Los Mas Valorados</h2>
+			<h2 className="subtitulo">Los Mas Valorados</h2>
 			<section className="MasVendidos">
-				{masValorados.slice(0, 5).map(p => (
+				{masValorados.slice(0, 3).map(p => (
 				<div className="producto-catalogo" key={p.id}>
 					<div className="imagen-producto">
-						<img src={`http://localhost:8000/storage/${p.imagen}`}/>
+						<img
+							src={`http://zent.es/${p.do_imagenes[0].url}`}
+							alt={p.titulo}
+						/>
 					</div>
 					<div className="info-producto">
-						<h3>{p.nombre}</h3>
-						<p>{p.precio}</p>
+						<h3>{p.titulo}</h3>
+						<p>{p.precio} €</p>
 					</div>
 				</div>
 				))}
-			</section> */}
+			</section>
 			<article className="Sponsor">
-				<Link to="/videojuegos"><img className="Promo" src="/imagesideas/promo.jpeg"></img></Link>
+				<Link to="/videojuegos"><img className="Promo" src="/imagesideas/Anuncio.jpeg"></img></Link>
 			</article>
+			<h2 className="subtitulo">Reservas</h2>
+			<section className="MasVendidos">
+				{productos.slice(0, 3).map(p => (
+				<div className="producto-catalogo" key={p.id}>
+					<div className="imagen-producto">
+						<img
+							src={`http://zent.es/${p.do_imagenes[0].url}`}
+							alt={p.titulo}
+						/>
+					</div>
+					<div className="info-producto">
+						<h3>{p.titulo}</h3>
+						<p>{p.precio} €</p>
+					</div>
+				</div>
+				))}
+			</section>
 		</main>
         </>
     );
