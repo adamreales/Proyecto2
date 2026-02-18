85133185739cbf07f77d17fbfce268c3dc6cc0a5
adamreales <adam.br2003@gmail.com>
2026-02-18 12:50:40 +0100

    Creacion de botones desplazamiento
    Creo los botones de desplazamiento de productos

--- React/tienda-react/src/components/SeccionProductos/SeccionProductos.jsx
+++ React/tienda-react/src/components/SeccionProductos/SeccionProductos.jsx
@@ -1,16 +1,49 @@
 import ProductoCard from "../ProductoCard/ProductoCard";
 import "./SeccionProductos.css";
+import { useState, useEffect } from "react";
+
 function SeccionProductos({ titulo, productos = [], reload = false }) {
+    const [startIndex, setStartIndex] = useState(0);
+    const maxitems = 3;
+
+    useEffect(() => {
+        setStartIndex(0);
+    }, [productos]);
+
+    const visibles = productos.slice(startIndex, startIndex + maxitems);
+
+    const next = () => {
+        if (startIndex + maxitems >= productos.length) {
+            setStartIndex(0);
+        } else {
+            setStartIndex(startIndex + maxitems);
+        }
+    };
+
+    const prev = () => {
+        if (startIndex === 0) {
+            const lastPage = Math.max(0, productos.length - maxitems);
+            setStartIndex(lastPage);
+        } else {
+            setStartIndex(startIndex - maxitems);
+        }
+    };
+
     return (
         <>
-        <h2 className="subtitulo">{titulo}</h2>
-        <div className="box-seccion">    
-            <section className="MasVendidos">
-                {productos.slice(0, 3).map(p => (
-                    <ProductoCard key={p.id} producto={p} reload={reload} />
-                ))}
-            </section>
-        </div>
+            <div className="cabezera">
+                <div><h2 className="subtitulo">{titulo}</h2></div>
+            <div className="botones"><button className="btn-prev" onClick={prev} disabled={startIndex === 0}><img src="/imagesideas/correrizq.png" className="mario" /></button>
+            <button className="btn-next" onClick={next} disabled={startIndex + maxitems >= productos.length}><img src="/imagesideas/correr.png" className="mario mario-right" /></button></div>
+            </div>
+            <div className="box-seccion">    
+                <section className="MasVendidos">
+                    {visibles.map(p => (
+                        <ProductoCard key={p.id} producto={p} reload={reload} />
+                    ))}
+
+                </section>
+            </div>
         </>
     );
 }
